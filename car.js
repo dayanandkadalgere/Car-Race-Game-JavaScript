9b597da6d448321b2b14040695d249d3bf454191
= <=>
2021-05-11 00:28:39 +0530

    wrote a style, 3 lines of HTML and created query selectors for the fielfs of html

---
+++ car.js
@@ -0,0 +1,9 @@
+const score = document.querySelector(".score");
+const startSreen = document.querySelector(".startScreen");
+const gameArea = document.querySelector(".gameArea");
+
+startSreen.addEventListener("click", start);
+
+function start(){
+    
+}
